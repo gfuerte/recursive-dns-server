918da9cf3ec94581e72e81bacfd66b32bb0653a8
Justin <rhodesju21@gmail.com>
2021-02-04 19:18:05 -0500

    Got initial server connection goin, still gonna test across other servers

--- client.py
+++ client.py
@@ -1,1 +1,37 @@
-print('client file')
+import threading
+import time
+import random
+import sys
+import socket
+
+if (len(sys.argv) < 4):
+    print("Invalid port number, please use the following to start the server: python client.py <rsHostname> <rsListenPort> <tsListenPort>")
+    exit()
+
+rsHost = sys.argv[1]
+rsPort = int(sys.argv[2])
+tsPort = int(sys.argv[3])
+#Establish server socket
+
+#Establish client/server connection
+try:
+    cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("[C]: Client socket created")
+except socket.error as err:
+    print('socket open error: {} \n'.format(err))
+    exit()
+
+# Define the port on which you want to connect to the server
+localhost_addr = socket.gethostbyname(socket.gethostname())
+
+# connect to the server on local machine
+server_binding = (rsHost, rsPort)
+cs.connect(server_binding)
+
+# Receive data from the server
+data_from_server=cs.recv(100)
+print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+
+# close the client socket
+cs.close()
+exit()
--- ts.py
+++ ts.py
@@ -1,1 +1,43 @@
-print('ts file')
+import threading
+import time
+import random
+import sys
+
+import socket
+
+if (len(sys.argv) < 2):
+    print("Invalid port number, please use the following to start the server: python ts.py <PORT_NUM>")
+    exit()
+
+portNum = int(sys.argv[1])
+#Establish server socket
+try:
+    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("[S]: Server socket created")
+except socket.error as err:
+    print('socket open error: {}\n'.format(err))
+    exit()
+
+server_binding = ('', portNum)
+ss.bind(server_binding)
+ss.listen(1)
+host = socket.gethostname()
+print("[S]: Server host name is {}".format(host))
+localhost_ip = (socket.gethostbyname(host))
+print("[S]: Server IP address is {}".format(localhost_ip))
+csockid, addr = ss.accept()
+print ("[S]: Got a connection request from a client at {}".format(addr))
+
+# send a intro message to the client.  
+msg = "Successfully connected, waiting for queries..."
+csockid.send(msg.encode('utf-8'))
+
+
+
+
+
+
+
+# Close the server socket
+ss.close()
+exit()
